3b1781ec158e3d35d190fc9ceca3ae159f7a142a
Sergievsky Nikita <53908805+yepIwt@users.noreply.github.com>
2022-11-22 00:15:58 +0300

    Добавил добавление нового записи в БД

--- db.py
+++ db.py
@@ -8,3 +8,10 @@ cursor = connection.cursor()
 def add_new(admin_id: int, chat_id: int):
 	cursor.execute(f"INSERT INTO data VALUES({admin_id}, {chat_id})")
 	connection.commit()
+
+
+def find_chat_id(chat_id: int) -> bool:
+	admind_id = cursor.execute(f"SELECT admin_id FROM data WHERE chat_id = {chat_id}").fetchall()
+	if admind_id:
+		return True
+	return False
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from vkwave.bots import SimpleLongPollBot
 import os
-
+import db
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = SimpleLongPollBot(tokens = BOT_TOKEN, group_id = 217315377)
 
+VK_MESSAGE_CONSTANT = 2000000000
 
-@bot.message_handler()
-def handle(_) -> str:
-    return "Hello world!"
+#@bot.message_handler(bot.text_filter("/start"))
+@bot.message_handler(bot.conversation_type_filter(from_what="from_chat"), bot.text_filter("/start"))
+def handle_start(msg_raw) -> str:
+    msg = """
+        Стартовая регистрация, которую нужно будет переделать!
+    """
+    print(msg_raw.object.object.message.peer_id)
+    if db.find_chat_id(chat_id = msg_raw.object.object.message.peer_id):
+        msg = "Братан, ты, походу, домом ошибся"
+    else:
+        db.add_new(msg_raw.object.object.message.from_id, msg_raw.object.object.message.peer_id)
+        msg = "Запомню тебя, братан"
+    return msg
 
+@bot.message_handler(bot.conversation_type_filter(from_what="from_chat"), bot.text_filter("/start"))
+def handle_time(msg_raw) -> str:
+    pass
 
 if __name__ == "__main__":
     bot.run_forever()
